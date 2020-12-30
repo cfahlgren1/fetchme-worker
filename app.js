bcdbc95ac5b91b430ed4af437175397ee1dd8aa3
Caleb Fahlgren <cfahlgren1@gmail.com>
2020-12-30 11:08:16 -0500

    removed api routes and other unneeded dependencies

--- app.js
+++ app.js
@@ -1,29 +1,15 @@
 const express = require("express");
 const kafka = require("./src/services/kafka/kafka");
 const connectDB = require("./src/database");
-const morgan = require("morgan");
-const admin = require("./src/admin/admin");
-
 const app = express();
-app.use(admin.adminBro.options.rootPath, admin.router);
-
-// log requests using morgan
-app.use(morgan("combined"));
 
 // Connect database
 connectDB();
 
 const port = 3000;
 
-// Define Routes
-app.use("/api/v1/user", require("./src/routes/api/v1/user"));
-app.use("/api/v1/auth", require("./src/routes/api/v1/auth"));
-app.use("/api/v1/profile", require("./src/routes/api/v1/profile"));
-app.use("/api/v1/requests", require("./src/routes/api/v1/requests"));
-app.use("/api/v1/teams", require("./src/routes/api/v1/teams"));
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/*", (req, res) => {
+  res.send("Hello World! - FetchMe Worker");
 });
 
 // start server
